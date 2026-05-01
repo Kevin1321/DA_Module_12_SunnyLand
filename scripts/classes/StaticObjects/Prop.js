8085173c699c050e04639acc063130f9329ac590
KevinR <kevinreich1321@gmail.com>
2026-05-01 18:14:12 +0200

    Feat: Background Level and prop implementation

--- scripts/classes/StaticObjects/Prop.js
+++ scripts/classes/StaticObjects/Prop.js
@@ -1,10 +1,12 @@
 class Prop extends GameObject {
-    constructor(positionX, positionY, sizeX, sizeY) {
+    constructor(context, positionX, positionY, sizeX, sizeY, imgSrc) {
         super(positionX, positionY, sizeX, sizeY);
+        this.img.src = imgSrc;
+        this.context = context;
     }
 
     OnTick(deltaTime) {
         super.OnTick(deltaTime);
-
+        this.context.drawImage(this.img, this.positionX, this.positionY, this.sizeX, this.sizeY);
     }
 }
