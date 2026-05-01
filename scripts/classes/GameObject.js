f9cbb85e2153c6bab7e72ff93d9c392e70826095
KevinR <kevinreich1321@gmail.com>
2026-05-01 18:15:59 +0200

    Ref: removed obsolete method and added fields for transform data

--- scripts/classes/GameObject.js
+++ scripts/classes/GameObject.js
@@ -1,11 +1,11 @@
 class GameObject {
     constructor(positionX, positionY, sizeX, sizeY) {
-        this.transform = new Transform(new Vector2(positionX, positionY), new Vector2(sizeX, sizeY));
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.transform = new Transform(new Vector2(this.positionX, this.positionY), new Vector2(this.sizeX, this.sizeY));
         this.img = new Image(this.sizeY, this.sizeX);
-    }
-
-    setImageByPath(path) {
-        this.img.src = path;
     }
 
     OnTick(deltaTime) {
