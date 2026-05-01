4316ed3cbbaffedb8b4979b8c9c048848458eda8
KevinR <kevinreich1321@gmail.com>
2026-05-01 15:35:54 +0200

    Add: Level class

---
+++ scripts/classes/StaticObjects/Level.js
@@ -0,0 +1,9 @@
+class Level extends GameObject {
+    constructor(positionX, positionY, sizeX, sizeY) {
+        super(positionX, positionY, sizeX, sizeY);
+    }
+
+    OnTick(deltaTime) {
+        super.OnTick(deltaTime);
+    }
+}
