af685f6299e6bc1e93b07c0209b3bfccfab8aa13
KevinR <kevinreich1321@gmail.com>
2026-05-04 14:56:15 +0200

    Feat: Added example objects for every type to test world

--- scripts/World.js
+++ scripts/World.js
@@ -37,21 +37,25 @@ class World {
     }
 
     CreateGameObjects() {
-        this.backgroundLayerOne = new Background(this.context, 0, 0, 720, 480, "assets/sprites/environments/Backgrounds/SunnyLandBase/back.png");
+        this.backgroundLayerOne = new Background(this.context, 0, 0, 720, 480, SpriteAssets.BACKGROUNDS.SUNNY_LAND_BASE);
 
-        this.middleOne = new Background(this.context, 0, 250, 176, 368, "assets/sprites/environments/Backgrounds/SunnyLandBase/middle.png");
-        this.middleTwo = new Background(this.context, 176, 250, 176, 368, "assets/sprites/environments/Backgrounds/SunnyLandBase/middle.png");
-        this.middleThree = new Background(this.context, 352, 250, 176, 368, "assets/sprites/environments/Backgrounds/SunnyLandBase/middle.png");
-        this.middleFour = new Background(this.context, 528, 250, 176, 368, "assets/sprites/environments/Backgrounds/SunnyLandBase/middle.png");
-        this.middleFive = new Background(this.context, 704, 250, 176, 368, "assets/sprites/environments/Backgrounds/SunnyLandBase/middle.png");
+        this.middleOne = new Background(this.context, 0, 250, 176, 368, SpriteAssets.MIDDLEGROUNDS.SUNNY_LAND_BASE);
+        this.middleTwo = new Background(this.context, 176, 250, 176, 368, SpriteAssets.MIDDLEGROUNDS.SUNNY_LAND_BASE);
+        this.middleThree = new Background(this.context, 352, 250, 176, 368, SpriteAssets.MIDDLEGROUNDS.SUNNY_LAND_BASE);
+        this.middleFour = new Background(this.context, 528, 250, 176, 368, SpriteAssets.MIDDLEGROUNDS.SUNNY_LAND_BASE);
+        this.middleFive = new Background(this.context, 704, 250, 176, 368, SpriteAssets.MIDDLEGROUNDS.SUNNY_LAND_BASE);
 
-        this.level_1 = new Level(this.context, 0, 384, 720, 96, "assets/sprites/environments/Level/Level_1.png");
+        this.level_1 = new Level(this.context, 0, 384, 720, 96, SpriteAssets.LEVEL.LEVEL_1);
 
-        this.woodenHouse = new Prop(this.context, 600, 302, 112, 98, "assets/sprites/environments/Props/wooden-house.png")
-        //draw grass seperatly
+        this.woodenHouse = new Prop(this.context, 600, 302, 112, 98, SpriteAssets.PROPS.WOODEN_HOUSE);
+        //todo: draw grass seperatly
 
         this.minion = new Minion(this.context, 200, 336, 64, 64);
+        this.boss = new Boss(this.context, 250, 272, 128, 128);
         this.player = new Player(this.context, 0, 336, 64, 64);
+
+        this.cherry_test = new Cherry(this.context, 100, 360, 32, 32);
+        this.gem_test = new Gem(this.context, 164, 360, 32, 32);
 
         this.gameObjects.push(this.backgroundLayerOne);
         this.gameObjects.push(this.middleOne);
@@ -65,6 +69,10 @@ class World {
 
         this.gameObjects.push(this.player);
         this.gameObjects.push(this.minion);
+        this.gameObjects.push(this.boss);
+
+        this.gameObjects.push(this.cherry_test);
+        this.gameObjects.push(this.gem_test);
     }
 
     CreateBackgrounds() {
