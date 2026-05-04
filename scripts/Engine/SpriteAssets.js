f88856c17a47e9304bf802a911214ba812ccaae3
KevinR <kevinreich1321@gmail.com>
2026-05-04 14:55:25 +0200

    Feat: Asset look up classes

--- scripts/Engine/SpriteAssets.js
+++ scripts/Engine/SpriteAssets.js
@@ -1,72 +1,122 @@
 class SpriteAssets {
 
     static PLAYER = {
-        IDLE_1 = "assets/sprites/characters/Foxy/idle/player-idle-1.png",
-        IDLE_2 = "assets/sprites/characters/Foxy/idle/player-idle-2.png",
-        IDLE_3 = "assets/sprites/characters/Foxy/idle/player-idle-3.png",
-        IDLE_4 = "assets/sprites/characters/Foxy/idle/player-idle-4.png",
+        IDLE_1: "assets/sprites/characters/Foxy/idle/player-idle-1.png",
+        IDLE_2: "assets/sprites/characters/Foxy/idle/player-idle-2.png",
+        IDLE_3: "assets/sprites/characters/Foxy/idle/player-idle-3.png",
+        IDLE_4: "assets/sprites/characters/Foxy/idle/player-idle-4.png",
 
-        LONG_IDLE = "assets/sprites/characters/Foxy/Hurt2/hurt-2.png",
+        LONG_IDLE: "assets/sprites/characters/Foxy/Hurt2/hurt-2.png",
 
-        JUMP_1 = "assets/sprites/characters/Foxy/jump/player-jump-1.png",
-        JUMP_2 = "assets/sprites/characters/Foxy/jump/player-jump-2.png",
+        JUMP_1: "assets/sprites/characters/Foxy/jump/player-jump-1.png",
+        JUMP_2: "assets/sprites/characters/Foxy/jump/player-jump-2.png",
 
-        RUN_1 = "assets/sprites/characters/Foxy/run/player-run-1.png",
-        RUN_2 = "assets/sprites/characters/Foxy/run/player-run-2.png",
-        RUN_3 = "assets/sprites/characters/Foxy/run/player-run-3.png",
-        RUN_4 = "assets/sprites/characters/Foxy/run/player-run-4.png",
-        RUN_5 = "assets/sprites/characters/Foxy/run/player-run-5.png",
-        RUN_6 = "assets/sprites/characters/Foxy/run/player-run-6.png",
+        RUN_1: "assets/sprites/characters/Foxy/run/player-run-1.png",
+        RUN_2: "assets/sprites/characters/Foxy/run/player-run-2.png",
+        RUN_3: "assets/sprites/characters/Foxy/run/player-run-3.png",
+        RUN_4: "assets/sprites/characters/Foxy/run/player-run-4.png",
+        RUN_5: "assets/sprites/characters/Foxy/run/player-run-5.png",
+        RUN_6: "assets/sprites/characters/Foxy/run/player-run-6.png",
 
-        VICTORY = "assets/sprites/characters/Foxy/Victory/Victory.png"
+        VICTORY: "assets/sprites/characters/Foxy/Victory/Victory.png"
     }
 
-    static SLIMER_ENEMY = {
-        IDLE_1 = "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle1.png",
-        IDLE_1 = "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle2.png",
-        IDLE_1 = "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle3.png",
-        IDLE_1 = "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle4.png",
-        IDLE_1 = "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle5.png",
-        IDLE_1 = "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle6.png",
-        IDLE_1 = "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle7.png",
-        IDLE_1 = "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle8.png",
+    static SLIMER = {
+        IDLE_1: "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle1.png",
+        IDLE_2: "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle2.png",
+        IDLE_3: "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle3.png",
+        IDLE_4: "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle4.png",
+        IDLE_5: "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle5.png",
+        IDLE_6: "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle6.png",
+        IDLE_7: "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle7.png",
+        IDLE_8: "assets/sprites/characters/Slimer/Slimer-Idle/slimer-idle8.png",
 
-        MOVE_1 = "DA_Module_12_SunnyLand/assets/sprites/characters/Slimer/Slimer-move/slimer1.png",
-        MOVE_1 = "DA_Module_12_SunnyLand/assets/sprites/characters/Slimer/Slimer-move/slimer2.png",
-        MOVE_1 = "DA_Module_12_SunnyLand/assets/sprites/characters/Slimer/Slimer-move/slimer3.png",
-        MOVE_1 = "DA_Module_12_SunnyLand/assets/sprites/characters/Slimer/Slimer-move/slimer4.png",
-        MOVE_1 = "DA_Module_12_SunnyLand/assets/sprites/characters/Slimer/Slimer-move/slimer5.png",
-        MOVE_1 = "DA_Module_12_SunnyLand/assets/sprites/characters/Slimer/Slimer-move/slimer6.png",
-        MOVE_1 = "DA_Module_12_SunnyLand/assets/sprites/characters/Slimer/Slimer-move/slimer7.png"
+        MOVE_1: "assets/sprites/characters/Slimer/Slimer-move/slimer1.png",
+        MOVE_2: "assets/sprites/characters/Slimer/Slimer-move/slimer2.png",
+        MOVE_3: "assets/sprites/characters/Slimer/Slimer-move/slimer3.png",
+        MOVE_4: "assets/sprites/characters/Slimer/Slimer-move/slimer4.png",
+        MOVE_5: "assets/sprites/characters/Slimer/Slimer-move/slimer5.png",
+        MOVE_6: "assets/sprites/characters/Slimer/Slimer-move/slimer6.png",
+        MOVE_7: "assets/sprites/characters/Slimer/Slimer-move/slimer7.png"
+    }
+
+    static VULTURE = {
+        IDLE_1: "assets/sprites/characters/Vulture/Vulture-Idle/vulture-idle1.png",
+        IDLE_2: "assets/sprites/characters/Vulture/Vulture-Idle/vulture-idle2.png",
+        IDLE_3: "assets/sprites/characters/Vulture/Vulture-Idle/vulture-idle3.png",
+        IDLE_4: "assets/sprites/characters/Vulture/Vulture-Idle/vulture-idle4.png",
+
+        FLYING_1: "assets/sprites/characters/Vulture/Vulture-Flying/vulture1.png",
+        FLYING_2: "assets/sprites/characters/Vulture/Vulture-Flying/vulture2.png",
+        FLYING_3: "assets/sprites/characters/Vulture/Vulture-Flying/vulture3.png",
+        FLYING_4: "assets/sprites/characters/Vulture/Vulture-Flying/vulture4.png"
     }
 
     static BACKGROUNDS = {
-        SUNNY_LAND_BASE = "assets/sprites/environments/Backgrounds/SunnyLandBase/back.png",
-        SUNNY_LAND_FOREST = "assets/sprites/environments/Backgrounds/SunnyLandForest/background.png"
+        SUNNY_LAND_BASE: "assets/sprites/environments/Backgrounds/SunnyLandBase/back.png",
+        SUNNY_LAND_FOREST: "assets/sprites/environments/Backgrounds/SunnyLandForest/background.png"
     }
 
     static MIDDLEGROUNDS = {
-        SUNNY_LAND_BASE = "assets/sprites/environments/Backgrounds/SunnyLandBase/middle.png",
-        SUNNY_LAND_FOREST = "assets/sprites/environments/Backgrounds/SunnyLandForest/middleground.png"
+        SUNNY_LAND_BASE: "assets/sprites/environments/Backgrounds/SunnyLandBase/middle.png",
+        SUNNY_LAND_FOREST: "assets/sprites/environments/Backgrounds/SunnyLandForest/middleground.png"
     }
 
     static LEVEL = {
-        LEVEL_1 = "assets/sprites/environments/Level/Level_1.png",
-        LEVEL_1_GRASS = ""
+        LEVEL_1: "assets/sprites/environments/Level/Level_1.png",
+        LEVEL_1_GRASS: ""
     }
 
     static PROPS = {
-        BIG_CRATE = "assets/sprites/environments/Props/big-crate.png",
-        BLOCK_BIG = "assets/sprites/environments/Props/block-big.png",
-        BLOCK = "assets/sprites/environments/Props/block.png",
-        BUSH = "assets/sprites/environments/Props/bush.png",
-        CRATE = "assets/sprites/environments/Props/crate.png",
-        ROCK = "assets/sprites/environments/Props/rock.png",
-        ROCK_1 = "assets/sprites/environments/Props/rock-1.png",
-        ROCK_2 = "assets/sprites/environments/Props/rock-2.png",
-        SHROOMS = "assets/sprites/environments/Props/shrooms.png",
-        SIGN = "assets/sprites/environments/Props/sign.png",
-        TREE = "assets/sprites/environments/Props/tree.png",
-        WOODEN_HOUSE = "assets/sprites/environments/Props/wooden-house.png"
+        BIG_CRATE: "assets/sprites/environments/Props/big-crate.png",
+        BLOCK_BIG: "assets/sprites/environments/Props/block-big.png",
+        BLOCK: "assets/sprites/environments/Props/block.png",
+        BUSH: "assets/sprites/environments/Props/bush.png",
+        CRATE: "assets/sprites/environments/Props/crate.png",
+        ROCK: "assets/sprites/environments/Props/rock.png",
+        ROCK_1: "assets/sprites/environments/Props/rock-1.png",
+        ROCK_2: "assets/sprites/environments/Props/rock-2.png",
+        SHROOMS: "assets/sprites/environments/Props/shrooms.png",
+        SIGN: "assets/sprites/environments/Props/sign.png",
+        TREE: "assets/sprites/environments/Props/tree.png",
+        WOODEN_HOUSE: "assets/sprites/environments/Props/wooden-house.png"
+    }
+
+    static PROJECTILS = {
+        STAR: "assets/sprites/Projectiles/star.png"
+    }
+
+    static PICK_UPS = {
+        CHERRY_1: "assets/sprites/PickUps/cherry/cherry-1.png",
+        CHERRY_2: "assets/sprites/PickUps/cherry/cherry-2.png",
+        CHERRY_3: "assets/sprites/PickUps/cherry/cherry-3.png",
+        CHERRY_4: "assets/sprites/PickUps/cherry/cherry-4.png",
+        CHERRY_5: "assets/sprites/PickUps/cherry/cherry-5.png",
+        CHERRY_6: "assets/sprites/PickUps/cherry/cherry-6.png",
+        CHERRY_7: "assets/sprites/PickUps/cherry/cherry-7.png",
+
+        GEM_1: "assets/sprites/PickUps/gem/gem-1.png",
+        GEM_2: "assets/sprites/PickUps/gem/gem-2.png",
+        GEM_3: "assets/sprites/PickUps/gem/gem-3.png",
+        GEM_4: "assets/sprites/PickUps/gem/gem-4.png",
+        GEM_5: "assets/sprites/PickUps/gem/gem-5.png",
+    }
+
+    static VFX = {
+        ENEMY_DEATH_1: "assets/sprites/VFX/enemy-death/enemy-death-1.png",
+        ENEMY_DEATH_2: "assets/sprites/VFX/enemy-death/enemy-death-2.png",
+        ENEMY_DEATH_3: "assets/sprites/VFX/enemy-death/enemy-death-3.png",
+        ENEMY_DEATH_4: "assets/sprites/VFX/enemy-death/enemy-death-4.png",
+        ENEMY_DEATH_5: "assets/sprites/VFX/enemy-death/enemy-death-5.png",
+        ENEMY_DEATH_6: "assets/sprites/VFX/enemy-death/enemy-death-6.png",
+
+        ITEM_FEEDBACK_1: "assets/sprites/VFX/item-feedback/item-feedback-1.png",
+        ITEM_FEEDBACK_2: "assets/sprites/VFX/item-feedback/item-feedback-2.png",
+        ITEM_FEEDBACK_3: "assets/sprites/VFX/item-feedback/item-feedback-3.png",
+        ITEM_FEEDBACK_4: "assets/sprites/VFX/item-feedback/item-feedback-4.png"
+    }
+
+    static UI = {
+        FOXY_PORTAIT: "assets/sprites/UI/foxy-portrait.png"
     }
 }
