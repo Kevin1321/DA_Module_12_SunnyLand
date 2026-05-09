5a1ff6c7d5424ade33e024725f8310213e86ca77
KevinR <kevinreich1321@gmail.com>
2026-05-09 17:50:13 +0200

    Feat: Added static ground var

--- scripts/GameObjects/StaticObjects/Level.js
+++ scripts/GameObjects/StaticObjects/Level.js
@@ -1,4 +1,6 @@
 class Level extends GameObject {
+    static GROUND = 336;
+
     constructor(context, positionX, positionY, sizeX, sizeY, imgSrc) {
         super(context, positionX, positionY, sizeX, sizeY);
         
