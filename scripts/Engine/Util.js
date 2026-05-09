73ad34023b97244bcc634c703ae82b28eec6b783
KevinR <kevinreich1321@gmail.com>
2026-05-09 17:43:25 +0200

    Add: Util class with static helpers

---
+++ scripts/Engine/Util.js
@@ -0,0 +1,9 @@
+class Util {
+    static Clamp(value, min, max) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    static GetRandomNormalized(){
+        return (Math.random() * 2 - 1).toFixed(1);
+    }
+}
