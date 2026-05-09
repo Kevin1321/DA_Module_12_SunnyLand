7fdf3b24140fc02c8db26a2cee54613dfebb0a55
KevinR <kevinreich1321@gmail.com>
2026-05-09 17:45:41 +0200

    Feat: Added event extension to Animation class

--- scripts/Engine/Animation.js
+++ scripts/Engine/Animation.js
@@ -1,15 +1,19 @@
-class Animation {
-    paths = [];
-    currentAnimationFrame = 0;
+class Animation extends EventTarget{
 
-    constructor(pathArray){
+    constructor(pathArray) {
+        super();
         this.paths = pathArray;
+        this.currentAnimationFrame = 0;
+        this.endOfAnimationEvent = new Event("EndOfAnimation")
     }
 
     nextFrame() {
         let returnIndex = this.currentAnimationFrame;
         this.currentAnimationFrame++;
-        if(this.currentAnimationFrame >= this.paths.length) this.currentAnimationFrame = 0;
+        if (this.currentAnimationFrame >= this.paths.length) {
+            this.dispatchEvent(this.endOfAnimationEvent);
+            this.currentAnimationFrame = 0;
+        }
 
         return this.paths[returnIndex];
     }
