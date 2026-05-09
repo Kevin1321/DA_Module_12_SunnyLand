7da6e5a876b1536d8338f14e210d80ce3a1e9c48
KevinR <kevinreich1321@gmail.com>
2026-05-09 17:50:43 +0200

    Ref: Added event to prevent space from scrolling to end of page

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,6 +1,12 @@
 let canvas;
 let world;
 
+window.addEventListener("keydown", (event) => {
+    if (event.code === "Space") {
+        event.preventDefault();
+    }
+});
+
 function main() {
     InputManager.Initialize();
     canvas = document.getElementById("game-canvas");
