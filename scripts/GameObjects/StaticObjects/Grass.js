adaf2de0d6d1678387199549645b2eb3280c2f61
KevinR <kevinreich1321@gmail.com>
2026-05-10 16:51:14 +0200

    Feat: new grass class to render grass seperatly

---
+++ scripts/GameObjects/StaticObjects/Grass.js
@@ -0,0 +1,12 @@
+class Grass extends GameObject {
+    constructor(context, positionX, positionY, sizeX, sizeY, imgSrc) {
+        super(context, positionX, positionY, sizeX, sizeY);
+        
+        this.img.src = imgSrc;
+    }
+
+    OnTick(frame, deltaTime) {
+        super.OnTick(frame, deltaTime);
+        this.context.drawImage(this.img, this.positionX, this.positionY, this.sizeX, this.sizeY);
+    }
+}
